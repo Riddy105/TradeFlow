43a5dd9fa7a84f8e21bbe3e4dd40cc04849f7529
Riddy105 <olaniranolatubosun@gmail.com>
2022-12-26 17:52:15 +0100

    making modifications

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 "use strict";
 const navEl = document.querySelector(`nav`);
+const mainEl = document.querySelector("main");
 const btnEl = document.querySelector(`.mobile-btn`);
 const faqBlocks = document.querySelectorAll(".faq-block");
 const faqBtns = document.querySelectorAll(".faq-button");
-
+const navBarContainer = document.querySelector(".nav-bar-container");
+const mainSectionCoords = mainEl.getBoundingClientRect();
 btnEl.addEventListener("click", () => {
   navEl.classList.toggle("nav-open");
 });
 
-faqBtns.forEach((faqBtn, index) => {
-  faqBtn.addEventListener("click", () => {
-    faqBlocks[index].classList.toggle("faq-open");
+window.addEventListener("scroll", () => {
+  window.scrollY > mainSectionCoords.top
+    ? navBarContainer.classList.add("fixed-nav")
+    : navBarContainer.classList.remove("fixed-nav");
+});
+
+faqBlocks.forEach((faqBlock, index) => {
+  faqBlock.addEventListener("click", () => {
+    faqBlock.classList.toggle("faq-open");
   });
 });
